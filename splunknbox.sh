1d550b5c1fc19e60b8953fe6e26cc68356114de5
Mohamad Hassan <mhassan@splunk.com>
2017-03-06 22:33:42 -0600

    Add files via upload

--- splunknbox.sh
+++ splunknbox.sh
@@ -3929,11 +3929,12 @@ if [ "$new" == "Y" ]; then
 	read -p "Newer version [$online_ver] available. Upgrade? [Y/n]? " answer
 	if [ -z "$answer" ] || [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
 		tput cup $LINES $(( ( $COLUMNS - ${#MESSAGE[10]} )  / 2 ))
-		echo "Upgrading and restarting ./${0##*/}                      "; sleep 4
+		echo "Upgrading ./${0##*/}                      "; sleep 4
 		curl -O "https://raw.githubusercontent.com/mhassan2/splunk-n-box/master/${0##*/}"
 		#curl --max-time 5 -O https://raw.github.com/mhassan2/splunk-n-box/master/${0##*/}	
 		chmod 755  ${0##*/}   	#set x permission on splunknbox.sh
-		./$(basename $0) && exit  # restart the script
+	#	./$(basename $0) && exit  # restart the script
+		echo "Please restart script!"
 
 	fi	
 fi
