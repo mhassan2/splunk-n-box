0c7f40578b8ac344f5faee17ae4fb7b057586eeb
mhassan2 <mhassan@splunk.com>
2017-12-29 18:27:26 -0600

    add color on version display

--- splunknbox.sh
+++ splunknbox.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-GIT_VERSION='$VERSION: [v4.3-30] $'
+GIT_VERSION='$VERSION: [v4.3-31] $'
 GIT_VERSION=`echo $GIT_VERSION| awk '{print $2}'|sed 's/\[\v//g'|sed 's/\]//g' `
 
-#	$VERSION: [v4.3-30] $
-#	$DATE:    [Fri Dec 29,2017 - 06:18:24PM -0600] $
+#################################################################################
+#	$VERSION: [v4.3-31] $
+#	$DATE:    [Fri Dec 29,2017 - 06:18:25PM -0600] $
 #	$AUTHOR:  [mhassan2 <mhassan@splunk.com>] $
 
 #################################################################################
@@ -346,10 +347,10 @@ elif [ "$FLIPFLOP" == 2 ] && [ "$curr_host" != "$prev_host" ]; then
         FLIPFLOP=0; COLOR="${LightCyan}"; echo > $CMDLOGBIN
 fi
 
-printf "${White}[$DATE:    [Fri Dec 29,2017 - 06:18:24PM -0600] $CMDLOGBIN
-printf "[$DATE:    [Fri Dec 29,2017 - 06:18:24PM -0600] $CMDLOGTXT
+printf "${White}[$DATE:    [Fri Dec 29,2017 - 06:18:25PM -0600] $CMDLOGBIN
+printf "[$DATE:    [Fri Dec 29,2017 - 06:18:25PM -0600] $CMDLOGTXT
 
-#echo "[$DATE:    [Fri Dec 29,2017 - 06:18:24PM -0600] $CMDLOGBIN
+#echo "[$DATE:    [Fri Dec 29,2017 - 06:18:25PM -0600] $CMDLOGBIN
 #sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" -i $CMDLOGBIN
 prev_host=$curr_host
 
