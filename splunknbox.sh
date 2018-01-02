4d60f2a7cf91cca0f8c2643e6feed19ecab2f405
mhassan2 <mhassan@splunk.com>
2018-01-02 09:35:49 -0600

    main script
    -

--- splunknbox.sh
+++ splunknbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #################################################################################
-#	__VERSION: 4.4-198 $
-#	__DATE: Tue Jan 02,2018 - 09:09:32AM -0600 $
+#	__VERSION: 4.4-199 $
+#	__DATE: Tue Jan 02,2018 - 09:09:33AM -0600 $
 #	__AUTHOR: mhassan2 <mhassan@splunk.com> $
 #################################################################################
 
@@ -1033,13 +1033,14 @@ else
 fi
 
 #detect_os is executed early so we place git stuff here
-GIT_VER=`echo "__VERSION: 4.4-198 $" | ggrep -Po "\d+.\d+-\d+"`
-GIT_DATE=`echo "__DATE: Tue Jan 02,2018 - 09:09:32AM -0600 $" | ggrep -Po "\w+\s\w+\s\d{2},\d{4}\s-\s\d{2}:\d{2}:\d{2}(AM|PM)\s-\d{4}" `
+GIT_VER=`echo "__VERSION: 4.4-199 $" | ggrep -Po "\d+.\d+-\d+"`
+GIT_DATE=`echo "__DATE: Tue Jan 02,2018 - 09:09:33AM -0600 $" | \
+		$GREP -Po "\w+\s\w+\s\d{2},\d{4}\s-\s\d{2}:\d{2}:\d{2}(AM|PM)\s-\d{4}" `
 GIT_AUTHOR=`echo "__AUTHOR: mhassan2 <mhassan@splunk.com> $GREP -Po "\w+\s\<\w+\@\w+.\w+\>"`
 
-#echo [$GIT_VER]
-#echo [$GIT_DATE]
-#echo [$GIT_AUTHOR]
+echo [$GIT_VER]
+echo [$GIT_DATE]
+echo [$GIT_AUTHOR]
 
 return 0
 }	#end detect_os()
