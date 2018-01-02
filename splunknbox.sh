851ab5dded85d8352c36984520c11eeae783e115
mhassan2 <mhassan@splunk.com>
2018-01-01 19:31:50 -0600

    main script
    -

--- splunknbox.sh
+++ splunknbox.sh
@@ -2,8 +2,8 @@
 
 
 #################################################################################
-#	__VERSION: 4.4-152 _
-#	__DATE: Mon Jan 01,2018 - 07:19:27PM -0600 _
+#	__VERSION: 4.4-153 _
+#	__DATE: Mon Jan 01,2018 - 07:19:28PM -0600 _
 #	__AUTHOR: mhassan2 <mhassan@splunk.com> _
 
 GIT_VERSION=`cat VERSION.TXT `	#VERSION.TXT should be present and current
