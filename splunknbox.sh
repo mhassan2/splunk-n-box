1373050ec8766fe7e0342b9f0a25f47eec233f29
mhassan2 <mhassan@splunk.com>
2018-01-02 11:12:57 -0600

    main script v4.4
    -

--- splunknbox.sh
+++ splunknbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #################################################################################
-#	__VERSION: 4.4-212 $
-#	__DATE: Tue Jan 02,2018 - 11:11:05AM -0600 $
+#	__VERSION: 4.4-213 $
+#	__DATE: Tue Jan 02,2018 - 11:11:08AM -0600 $
 #	__AUTHOR: mhassan2 <mhassan@splunk.com> $
 #################################################################################
 
@@ -1035,9 +1035,9 @@ fi
 #detect_os is executed early so we place git stuff here
 #Must break the line with \ otherwise git clean/smudge scripts will
 #screw up things if the $ sign is not the last char
-GIT_VER=`echo "__VERSION: 4.4-212 $" | \
+GIT_VER=`echo "__VERSION: 4.4-213 $" | \
 		$GREP -Po "\d+.\d+-\d+"`
-GIT_DATE=`echo "__DATE: Tue Jan 02,2018 - 11:11:05AM -0600 $" | \
+GIT_DATE=`echo "__DATE: Tue Jan 02,2018 - 11:11:08AM -0600 $" | \
 		$GREP -Po "\w+\s\w+\s\d{2},\d{4}\s-\s\d{2}:\d{2}:\d{2}(AM|PM)\s-\d{4}" `
 GIT_AUTHOR=`echo "__AUTHOR: mhassan2 <mhassan@splunk.com> $" | \
 		$GREP -Po "\w+\s\<\w+\@\w+.\w+\>"`
