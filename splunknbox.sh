d1221d3b46bf472df7f2bd104d7793969e739a50
Mohamad Hassan <mhassan@splunk.com>
2018-10-08 14:23:01 -0500

    version 5.0

--- splunknbox.sh
+++ splunknbox.sh
@@ -6133,7 +6133,6 @@ fi
 display_welcome_screen		#ggrep must installed otherwise ver check will fail
 main_menu_inputs
 
-
 #<<<<<<<<<<<<<----------   MAIN ENDS     ---------->>>>>>>>>>>>
 
 ##### EOF #######
